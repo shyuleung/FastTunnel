3679dccf2693cf0280c861859ab5db373f712741
shyuleung <43627625+shyuleung@users.noreply.github.com>
2023-04-02 22:59:15 +0900

    徐　修正
    案内追加

--- FastTunnel.Server/install.bat
+++ FastTunnel.Server/install.bat
@@ -2,6 +2,7 @@ CHCP 65001
 @echo off
 color 0e
 @echo ==================================
+@echo =================じょ=================
 @echo 提醒：请右键本文件，用管理员方式打开。
 @echo ==================================
 @echo Start Install FastTunnel.Server
